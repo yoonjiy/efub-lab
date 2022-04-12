d911bb83604e2d0a50240c5dbd88d8b6d852bd7d
YOONJI KIM <stritegdc@gmail.com>
2022-04-12 19:49:33 +0900

    [feat]: 이펍 3주차 세미나 프로젝트 생성

---
+++ seminar03/src/main/java/com/efub/seminar03/Seminar03Application.java
@@ -0,0 +1,13 @@
+package com.efub.seminar03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminar03Application.class, args);
+	}
+
+}
---
+++ seminar03/src/test/java/com/efub/seminar03/Seminar03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efub.seminar03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
